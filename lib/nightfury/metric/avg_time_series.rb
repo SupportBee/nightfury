922c307c51116981f6d4c684e0bcb2024b21f616
Avinasha Shastry <me@avinasha.com>
2013-01-30 12:55:14 +0530

    Fixing the wrong calculation of avg

--- lib/nightfury/metric/avg_time_series.rb
+++ lib/nightfury/metric/avg_time_series.rb
@@ -3,17 +3,17 @@ module Nightfury
     class AvgTimeSeries < TimeSeries
 
       def default_meta
-        {'count' => 0}
+        {'count' => 0.0, 'total' => 0.0}
       end
 
       protected
 
       def before_set(value)
-        count = meta['count'] + 1
-        raw = get
-        current_value = raw.nil? ? 0 : raw.values.first.to_f
-        result = (current_value + value) / count.to_f
-        meta['count'] = count
+        updated_count = meta['count'].to_f + 1.0
+        updated_total = meta['total'].to_f + value
+        result = updated_total / updated_count
+        meta['count'] = updated_count
+        meta['total'] = updated_total
         save_meta
         result
       end
--- spec/nightfury/metric/avg_time_series_spec.rb
+++ spec/nightfury/metric/avg_time_series_spec.rb
@@ -3,14 +3,16 @@ require 'spec_helper'
 describe Nightfury::Metric::AvgTimeSeries do
   it "should have the right default meta" do
     avg_metric = Nightfury::Metric::AvgTimeSeries.new(:avg)
-    avg_metric.default_meta.should == { 'count' => 0 }
+    avg_metric.default_meta.should == { 'count' => 0.0, 'total' => 0.0 }
   end
 
   it "should calculate avg before saving" do
     avg_metric = Nightfury::Metric::AvgTimeSeries.new(:avg)
-    avg_metric.set(1)
+    avg_metric.set(3)
     avg_metric.set(2)
-    avg_metric.get.values.first.should == "1.5"
+    avg_metric.set(4)
+    avg_metric.set(11)
+    avg_metric.get.values.first.should == "5.0"
   end
 
   it "should update meta update count" do
