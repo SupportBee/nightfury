25e4513c6ce216944739676c2475d10484fd7fc3
Avinasha Shastry <me@avinasha.com>
2013-09-26 11:02:44 +0530

    Update example.rb

--- example.rb
+++ example.rb
@@ -11,8 +11,9 @@ class CompanyIdentity < Nightfury::Identity::Base
   # Defining metric: 
   # #metric(name, type)
   # type defaults to :value
-  metric :first_response_time, :time_series, step: :day
-  metric :ticket_count
+  metric :first_response_time, :avg_time_series, step: :day
+  metric :tickets_count, :count_time_series, step: :day, store_as: :tc
+  metric :number_of_users
 end
 
 
@@ -39,13 +40,13 @@ puts "#get_all; returns all values \n #{c.first_response_time.get_all}"
 
 
 # Working with values
-c.ticket_count.set(0)
+c.number_of_users.set(0)
 # increment by 1
-c.ticket_count.incr
+c.number_of_users.incr
 # increment by 10
-c.ticket_count.incr(10)
+c.number_of_users.incr(10)
 # decrement by 1
-c.ticket_count.decr
+c.number_of_users.decr
 # decrement by 5
-c.ticket_count.decr(5)
+c.number_of_users.decr(5)
 puts "Ticket Count; should be 5 \n #{c.ticket_count.get}"
