7138f1dd16053b1a6d928fe856d35be5ec6b4b0b
Nisanth Chunduru <nisanth074@gmail.com>
2019-09-04 15:35:47 +0530

    Support rails 4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,24 +8,25 @@ require 'pry'
 
 # Start our own redis-server to avoid corrupting any others
 REDIS_BIN  = 'redis-server'
-REDIS_PORT = ENV['REDIS_PORT'] || 9212
+REDIS_PORT = ENV['REDIS_PORT'] || 6379
 REDIS_HOST = ENV['REDIS_HOST'] || 'localhost'
 REDIS_PID  = File.expand_path 'redis.pid', File.dirname(__FILE__)
 REDIS_DUMP = File.expand_path 'redis.rdb', File.dirname(__FILE__)
-puts "=> Starting redis-server on #{REDIS_HOST}:#{REDIS_PORT}"
-fork_pid = fork do
-  system "(echo port #{REDIS_PORT}; echo logfile /dev/null; echo daemonize yes; echo pidfile #{REDIS_PID}; echo dbfilename #{REDIS_DUMP}) | #{REDIS_BIN} -"
-end
-at_exit do
-  pid = File.read(REDIS_PID).to_i
-  puts "=> Killing #{REDIS_BIN} with pid #{pid}"
-  Process.kill "TERM", pid
-  Process.kill "KILL", pid
-  File.unlink REDIS_PID
-  File.unlink REDIS_DUMP if File.exists? REDIS_DUMP
-end
 
-REDIS_CLIENT = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT)
+# puts "=> Starting redis-server on #{REDIS_HOST}:#{REDIS_PORT}"
+# fork_pid = fork do
+#   system "(echo port #{REDIS_PORT}; echo logfile /dev/null; echo daemonize yes; echo pidfile #{REDIS_PID}; echo dbfilename #{REDIS_DUMP}) | #{REDIS_BIN} -"
+# end
+# at_exit do
+#   pid = File.read(REDIS_PID).to_i
+#   puts "=> Killing #{REDIS_BIN} with pid #{pid}"
+#   Process.kill "TERM", pid
+#   Process.kill "KILL", pid
+#   File.unlink REDIS_PID
+#   File.unlink REDIS_DUMP if File.exists? REDIS_DUMP
+# end
+
+REDIS_CLIENT = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT, :db => 3)
 Redis.current = REDIS_CLIENT
 
 RSpec.configure do |config|
